3bf0628a0a6a7ca9e3eed0794936e528ce3f6ce3
scripter <t279507736@outlook.com>
2016-04-16 02:34:34 +0800

    Added files via upload

---
+++ solider.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_game
+{
+    public class exs
+    {
+        public List<solider> li;
+        private List<int> a;
+
+
+    }
+
+     public class solider
+    {
+        int attack;
+        int defence;
+        int armor;
+        int health;
+        int demage;
+        bool IsAlive { get; set; }
+        Random ran;                 
+        public solider()
+        {
+            ran = new Random();
+        }
+        public void attackto(solider beacttacked)
+        {
+            beacttacked.beattack(attack, demage);
+        }
+        public void beattack(int attack, int demage)
+        {
+          double refer=(figure(attack, this.defence)*100);
+            if ((ran.Next() % 100) < refer)
+                health-=demage;
+        }
+        private double figure(int attack, int defense)
+        {
+
+           
+            return 0;
+        }
+        private int DemageJudge(int attack, int aromor)
+        {
+            if (demage < 0 || attack < 0)
+                return 0;
+            if (demage > aromor)
+                return (demage - aromor / 2);
+            else return (demage*demage/aromor);
+        }
+
+    }
+    
+}
